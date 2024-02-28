2562cbaea5761fd9796d17123c3c755d7089e832
Maurício <itoalberti@gmail.com>
2024-02-28 17:44:36 -0300

    updates

---
+++ frontend/src/dados/mockClientes.js
@@ -0,0 +1,7 @@
+var listaContas = [
+  { nome: 'Pedro Bento', cpf: '12345678900', dataNasc: '01/02/88', endereco: 'Rua Lambari Azul, nº 445', cidade: 'Assis', uf: 'sp', email: 'pedro@email.com', tel: '18991234567' },
+  { nome: 'Chico Assunção', cpf: '33344455566', dataNasc: '11/11/11', endereco: 'Rua das Bromélias, nº 12', cidade: 'Confins', uf: 'mg', email: 'chico@email.com', tel: '18991234567' },
+  { nome: 'Zuleica Bragança', cpf: '98976754533', dataNasc: '06/07/08', endereco: 'Rua Lambari Azul, nº 445', cidade: 'Assis', uf: 'sp', email: 'pedro@email.com', tel: '18991234567' },
+];
+
+export default listaContas;
---
+++ frontend/src/telas/TelaCadastrarCliente.jsx
@@ -0,0 +1,135 @@
+import { Button, Col, Dropdown, Form, Row } from 'react-bootstrap';
+import Pagina from '../templates/Pagina';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useState } from 'react';
+// import listaContas from '../dados/mockContas.js';
+
+export default function TelaCadastrarCliente(props) {
+  const [validado, setValidado] = useState(false);
+  const [cliente, setCliente] = useState({
+    // incluir código da agência → autoincrement, não definido pelo usuário
+    nome: '',
+    cpf: '',
+    dataNasc: '',
+    endereco: '',
+    cidade: '',
+    uf: '',
+    email: '',
+    telefone: '',
+  });
+
+  function manipularMudanca(e) {
+    const elemForm = e.currentTarget;
+    const id = elemForm.id;
+    const valor = elemForm.value;
+    setCliente({ ...cliente, [id]: valor });
+  }
+
+  function manipulaSubmissao(e) {
+    const form = e.currentTarget;
+    if (form.checkValidity()) {
+      // dados válidos → proceder com o cadastro
+      let clientes = props.listaClientes;
+      clientes.push(cliente);
+      props.setAgencia(clientes);
+      setValidado(false);
+      props.exibirTabela(true);
+    } else {
+      setValidado(true);
+    }
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  //
+  // RETURN
+  //
+  return (
+    <>
+      <Pagina>
+        <h2>Cadastro de nova agência</h2>
+        <br />
+        <Form noValidate validated={validado} onSubmit={manipulaSubmissao}>
+          {/* ENDEREÇO */}
+          <Form.Group className='mb-3' controlId='endereco' style={{ width: '340px' }}>
+            <Form.Label>Endereço:</Form.Label>
+            <Form.Control required type='text' id='endereco' value={cliente.endereco} onChange={manipularMudanca} />
+            <Form.Control.Feedback type='invalid'>Informe o endereço da agência!</Form.Control.Feedback>
+          </Form.Group>
+
+          {/* CIDADE */}
+          <Form.Group className='mb-3' controlId='cidade' style={{ width: '340px' }}>
+            <Form.Label>Cidade:</Form.Label>
+            <Form.Control required type='email' id='cidade' value={cliente.cidade} onChange={manipularMudanca} />
+            <Form.Control.Feedback type='invalid'>Informe a cidade da agência!</Form.Control.Feedback>
+          </Form.Group>
+
+          <Row>
+            {/* UF */}
+            <Col md='2'>
+              <Form.Group className='mb-3' controlId='uf' style={{ width: '150px' }}>
+                <Form.Label style={{ width: '50px' }}>UF:</Form.Label>
+                <select className='mb-3' style={{ width: '40px' }} id='uf'>
+                  <option></option>
+                  <option value='SP'>SP</option>
+                  <option value='SP'>PR</option>
+                  <option value='SP'>MG</option>
+                </select>
+                {/* <Dropdown.Toggle required id='uf'>
+                  akjsdh
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href='AC'>AC</Dropdown.Item>
+                  <Dropdown.Item>AL</Dropdown.Item>
+                  <Dropdown.Item>AP</Dropdown.Item>
+                  <Dropdown.Item>AM</Dropdown.Item>
+                  <Dropdown.Item>BA</Dropdown.Item>
+                  <Dropdown.Item>CE</Dropdown.Item>
+                  <Dropdown.Item>ES</Dropdown.Item>
+                  <Dropdown.Item>GO</Dropdown.Item>
+                  <Dropdown.Item>MA</Dropdown.Item>
+                  <Dropdown.Item>MT</Dropdown.Item>
+                  <Dropdown.Item>MS</Dropdown.Item>
+                  <Dropdown.Item>MG</Dropdown.Item>
+                  <Dropdown.Item>PA</Dropdown.Item>
+                  <Dropdown.Item>PB</Dropdown.Item>
+                  <Dropdown.Item>PR</Dropdown.Item>
+                  <Dropdown.Item>PE</Dropdown.Item>
+                  <Dropdown.Item>PI</Dropdown.Item>
+                  <Dropdown.Item>RJ</Dropdown.Item>
+                  <Dropdown.Item>RN</Dropdown.Item>
+                  <Dropdown.Item>RS</Dropdown.Item>
+                  <Dropdown.Item>RO</Dropdown.Item>
+                  <Dropdown.Item>RR</Dropdown.Item>
+                  <Dropdown.Item>SC</Dropdown.Item>
+                  <Dropdown.Item>SP</Dropdown.Item>
+                  <Dropdown.Item>SE</Dropdown.Item>
+                  <Dropdown.Item>TO</Dropdown.Item>
+                  <Dropdown.Item>DF</Dropdown.Item>
+                </Dropdown.Menu> */}
+                <Form.Control.Feedback type='invalid'>Informe o estado da agência!</Form.Control.Feedback>
+              </Form.Group>
+            </Col>
+          </Row>
+
+          <br />
+          <Row>
+            {/* BOTÃO DE CADASTRAR */}
+            <Col xs='auto'>
+              <Button variant='dark' type='submit'>
+                Cadastrar agência
+              </Button>
+            </Col>
+
+            {/* BOTÃO DE CANCELAR */}
+            <Col xs='auto'>
+              <LinkContainer to='/'>
+                <Button variant='secondary'>Cancelar</Button>
+              </LinkContainer>
+            </Col>
+          </Row>
+        </Form>
+      </Pagina>
+    </>
+  );
+}
